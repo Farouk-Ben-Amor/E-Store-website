172eeb9094277eccc49667a5cf93bf6466c02347
Farouk-Ben-Amor <faaroukbnamor@gmail.com>
2023-04-10 18:08:07 +0100

    1st commit

---
+++ users/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render , redirect
+from item.models import Category , Item
+
+from .forms import SignUpForm
+
+def index(request):
+    items = Item.objects.filter(is_sold = False)[0:6]
+    categories = Category.objects.all()
+    return render(request, 'users/index.html',{'categories' : categories , 'items' : items})
+
+def contact(request) :
+    return render(request, 'users/contact.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else :
+        form = SignUpForm()
+    return render(request , 'users/signup.html' , {'form' : form } )
+
